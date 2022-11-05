f3ea4330c227f2c18d2156a9085c0a8cba6d774b
wyuan19asu <wyuan19@asu.edu>
2022-11-05 15:37:43 -0700

    updated comments

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 const buttonSquares = document.querySelectorAll(".board__square");
 const title = document.querySelector(".board__title");
-console.log(buttonSquares);
 
 let currentPlayer = "X";
 let gameOver = false;
@@ -8,10 +7,14 @@ let board = new Array(9);
 
 buttonSquares.forEach((square, i) => {
     square.addEventListener("click", () => {
+        // keep changing x/o if we keep clicking. Checks if its empty, if not then return(exit) 
+        //to not run the rest of code
         if (square.innerHTML || gameOver) {
             return;
         }
+        // sets the innerHTML in the square to be current player
         square.innerHTML = currentPlayer;
+        // sets the element in the arr to the currplayer
         board[i] = currentPlayer;
 
         if (checkWin()) {
@@ -24,6 +27,7 @@ buttonSquares.forEach((square, i) => {
             return gameOver = true;
         }
 
+        // current player, if its X then switch to an O for next player
         currentPlayer = currentPlayer === "X" ? "O" : "X";
         title.innerHTML = `${currentPlayer}'s Turn`;
     })
@@ -42,6 +46,7 @@ function checkDraw() {
 
     // Method 1
     for (let i = 0; i < board.length; i++) {
+        // if any element in this board does not exist then false 
         if (!board[i]) {
             return false;
         }
@@ -85,12 +90,14 @@ function checkWin() {
     //     console.log(indices);
     // })
 
+    // checks winning indices compared to the symbol on the board
     for (let i = 0; i < winningIndices.length; i++) {
         const matchingIndices = winningIndices[i];
         let symbol1 = board[matchingIndices[0]];
         let symbol2 = board[matchingIndices[1]];
         let symbol3 = board[matchingIndices[2]];
 
+        // if any are empty, no winning state then continue
         if (!symbol1 || !symbol2 || !symbol3) {
             continue;
         }
